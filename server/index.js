63bfc1f05a59839e6e00d2580f81ad1594f292e7
KrishnaSrikanth Donthineni <dkrishnasrikanth@gmail.com>
2020-02-18 09:11:52 -0500

    week4-mon

---
+++ server/index.js
@@ -0,0 +1,10 @@
+
+
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('krishna srikanth donthineni'))
+
+app.listen(port, () => console.log(`listening at http://localhost:${port}`))
