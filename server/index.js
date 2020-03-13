d90ec2d8c339ed44dc5c27432a3fb797c611fc82
KrishnaSrikanth Donthineni <dkrishnasrikanth@gmail.com>
2020-03-13 14:00:45 -0400

    week 8 update

--- server/index.js
+++ server/index.js
@@ -9,7 +9,13 @@ const port = 3000;
 app
     .use(express.json())
     .use(express.urlencoded({extended: true }))
+    .use(express.static(__dirname + '/../client/dist'))
     .get('/', (req, res) => res.send('This class is awesome!') )
     .use('/game',gameController)
+
+    .use((req,res) => {
+        const homePath =path.join( __dirname,'/../client/dist/index.html');
+        console.log()
+    })
 
 app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
